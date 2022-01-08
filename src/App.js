afe163403911b34c6f22ad5a86a6cbd27d732ae3
Nicolas Rodriguez Salguero <nicolas.salguero@rappi.com>
2022-01-08 17:44:50 -0500

    Create initial template

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Bienvenido a Cyber Place
+        </p>
+        <a
+          className="App-link"
+          href="https://bit.ly/3n9qNeK"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Comencemos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
