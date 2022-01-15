81478b9b83bf04a755c0f956daba2e56bfa7a012
Nicolas Rodriguez Salguero <nicolas.salguero@rappi.com>
2022-01-15 16:13:47 -0500

    Mostrar productos en apartado de compra

--- src/components/products/buy-product/buy.component.jsx
+++ src/components/products/buy-product/buy.component.jsx
@@ -1,29 +1,29 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import dataService from '../../../services/product.service'
 
 const BuyProduct = () => {
-  
-  const [datos, setDatos] = useState({
-    name: '',
-    price: 0,
-    description: '',
-    stars: 0
-  })
-  
-  const [status, setStatus] = useState('  ')
-  
-  useEffect((datos) => {
-    dataService.getAllProducts(datos,setStatus);
-    console.log(datos);
-  }, []); 
+
+  const [datos, setDatos] = useState([])
+
+  useEffect(() => {
+    dataService.getAllProducts(setDatos);
+  },[]);
+
+  const data = datos.map((datos) => <tr>
+  <td><img src={datos.img_url} alt="imagen" height={150}  /></td>
+  <td>{datos.name}</td>
+  <td>{datos.price}</td>
+  <td>{datos.description}</td>
+  <td>{datos.stars}</td>
+  </tr>);
 
   return (
     <>
       <h2>Selecciona tu producto para comprar </h2>
-      <table className="table">
+      <table className="table" >
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col">Imagen</th>
             <th scope="col">Nombre</th>
             <th scope="col">Precio</th>
             <th scope="col">Descripción</th>
@@ -31,13 +31,7 @@ const BuyProduct = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Draconic</td>
-            <td>50</td>
-            <td>Teclado</td>
-            <td>5</td>
-          </tr>
+            {data}
         </tbody>
       </table>
     </>
