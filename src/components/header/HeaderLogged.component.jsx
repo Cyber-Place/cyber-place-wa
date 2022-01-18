74da2bf304987394f7b1cbb7ac3be6a0cf3b72c4
dmendozam <dmendozam@unal.edu.co>
2022-01-18 09:13:14 -0500

    change to docker-compose. searchbar

--- src/components/header/HeaderLogged.component.jsx
+++ src/components/header/HeaderLogged.component.jsx
@@ -1,11 +1,37 @@
-
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 function HeaderLogged(params) {
+  //Esto es solo para ejemplo
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 }];
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="/">
         <img src={require('./logo.png')} width='50px' alt='Logo' />
         | Cyber Place</a>
+      
+      <div style={{width:'60%'}}>
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => 
+        <TextField {...params} label="" variant="outlined" InputLabelProps={{shrink: false}}
+        sx={{
+          backgroundColor:'white',
+          borderRadius:'4px',
+   
+        }}
+        
+        />}
+      />
+      
+      </div>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
