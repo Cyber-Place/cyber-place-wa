e158e51745b46d9b3bcb5d9965ccb950eadb3594
dmendozam <dmendozam@unal.edu.co>
2022-01-19 21:40:25 -0500

    NAvbar toggler gone.

--- src/components/header/HeaderUnlogged.js
+++ src/components/header/HeaderUnlogged.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchBar from './SearchBar';
 
-
 const HeaderUnlogged = () => {
   
     return (
@@ -14,15 +13,9 @@ const HeaderUnlogged = () => {
               <span className="ms-2 fs-4">CyberPlace</span>
             </Link>
             <SearchBar/>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav ms-4" align="center">
-                <Link to="/product/buy" className="nav-item nav-link px-3">Productos</Link>
+              <div className="navbar-nav">
                 <Link to="/shopping-cart" className="nav-item nav-link px-3" ><ShoppingCartIcon/></Link>
               </div>
-            </div>
             <form className="d-flex" >
               <div className="navbar-nav me-4" align="center">
                 <Link to="/login" className="nav-item nav-link px-3" >
